e1c63f9b3f10a04207a48051e406cd8c82f3d88c
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-17 23:45:04 +0200

    Created CRUD system v0.0.2

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Blog
 
 # Create your views here.
 
 def home(request):
     return render(request, 'crud/home.html')
+
+class CreateBlog_page(generic.CreateView):
+    model = Blog
+    fields = ['title', 'body']
+    template_name = 'crud/create_page.html'
+    success_url = reverse_lazy('home')
+
+class DetailBlog_page(generic.DetailView):
+    model = Blog
+    template_name = 'crud/detail_page.html'
+
+class UpdateBlog_page(generic.UpdateView):
+    model = Blog
+    template_name = 'crud/update_page.html'
+    fields = ['title', 'body']
+    success_url = reverse_lazy('home')
+
+class DeleteBlog_page(generic.DeleteView):
+    model = Blog
+    template_name = 'crud/delete_page.html'
+    success_url = reverse_lazy('home')
--- django_crud/urls.py
+++ django_crud/urls.py
@@ -20,4 +20,9 @@ from crud import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    path('create/', views.CreateBlog_page.as_view(), name="create_blog_page"),
+    path('<int:pk>/', views.DetailBlog_page.as_view(), name="detail_blog_page"),
+    path('<int:pk>/update/', views.UpdateBlog_page.as_view(), name="update_blog_page"),
+    path('<int:pk>/delete/', views.DeleteBlog_page.as_view(), name="delete_blog_page"),
 ]
